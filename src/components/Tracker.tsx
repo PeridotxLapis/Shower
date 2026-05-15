05da11f067d876ac8b00486b06804ea82a16ebe1
PeridotxLapis <populeeorange@gmail.com>
2026-05-15 10:19:01 -0700

    Update Tracker.tsx

--- src/components/Tracker.tsx
+++ src/components/Tracker.tsx
@@ -50,7 +50,7 @@ export default function Tracker() {
   const formatTime = (seconds: number) => {
     const mins = Math.floor(seconds / 60);
     const secs = seconds % 60;
-    return `${mins}:${secs < 10 ? '0' : ''}${secs}`;
+    return `\${mins}:\${secs < 10 ? '0' : ''}\${secs}`;
   };
 
   return (
